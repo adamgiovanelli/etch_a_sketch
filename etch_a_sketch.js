2ad519ee9f7e5609b2d6a660ea1eb5fcc119cba3
adamgiovanelli <adamgiovanelli@gmail.com>
2023-02-21 10:29:52 -0500

    added event listener and mouseover function to turn blocks black

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,15 +1,32 @@
+//Creates grid blocks inside the etch a sketch. Input is number of columns/rows
 function createDivs(dim) {
     
     numOfBlocks = dim*dim;
     gridContainer.setAttribute('style', 'grid-template-columns: repeat(' + dim + ' , 1fr);')
 
     for (i=0;i<numOfBlocks;i++){
+        const blockName = 'block'+i;
         const block = document.createElement('div');
-        /*block.textContent = i;*/
         block.classList.add('gridItems');
+        block.setAttribute('id', blockName);
         gridContainer.appendChild(block);
+
     }
 
 }
 
-createDivs(10);
+createDivs(50);
+
+const allBlocks = document.querySelectorAll('.gridItems');
+
+//mouseover turns blocks black
+function applyMouseover () {
+    console.log('mouseover');
+    this.style.backgroundColor ='black';
+}
+
+//applies mouseover function to all blocks
+for (i=0;i<allBlocks.length;i++){
+    allBlocks[i].addEventListener('mouseover', applyMouseover);
+}
+
